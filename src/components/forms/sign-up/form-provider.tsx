15d11008c5dd3b9055afc11a72ecefa727fc6c4c
ariz565 <mohdariz.phy@gmail.com>
2025-01-27 20:37:47 +0530

    feat: Added the required components

--- src/components/forms/sign-up/form-provider.tsx
+++ src/components/forms/sign-up/form-provider.tsx
@@ -1,17 +1,31 @@
-import { AuthContextProvider } from "@/context/use-auth-context";
-import React from "react";
-import { FormProvider } from "react-hook-form";
+'use client'
+import { Loader } from '@/components/loader'
+import { AuthContextProvider } from '@/context/use-auth-context'
+import { useSignUpForm } from '@/hooks/sign-up/use-sign-up'
+import React from 'react'
+import { FormProvider } from 'react-hook-form'
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 const SignUpFormProvider = ({ children }: Props) => {
+  const { methods, onHandleSubmit, loading } = useSignUpForm()
+
   return (
     <AuthContextProvider>
-      <FormProvider></FormProvider>
+      <FormProvider {...methods}>
+        <form
+          onSubmit={onHandleSubmit}
+          className="h-full"
+        >
+          <div className="flex flex-col justify-between gap-3 h-full">
+            <Loader loading={loading}>{children}</Loader>
+          </div>
+        </form>
+      </FormProvider>
     </AuthContextProvider>
-  );
-};
+  )
+}
 
-export default SignUpFormProvider;
+export default SignUpFormProvider
